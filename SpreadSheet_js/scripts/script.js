77c2ca9375d85fc81c2f856cb357e8c633d000d5
dbgus1006 <dbgus1006@gmail.com>
2023-11-03 23:34:19 +0900

    update header style

--- SpreadSheet_js/scripts/script.js
+++ SpreadSheet_js/scripts/script.js
@@ -2,14 +2,17 @@ const spreadSheetContainer = document.querySelector("#spreadsheet-container");
 const ROWS = 10;
 const COLS = 10;
 const spreadsheet = [];
+const alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 class Cell {
-    constructor(isHeader, disabled, data, row, column, active = false) {
+    constructor(isHeader, disabled, data, row, column, rowName, columnName, active = false) {
         this.isHeader = isHeader;
         this.disabled = disabled;
         this.data = data;
         this.row = row;
+        this.rowName = rowName;
         this.column = column;
+        this.columnName = columnName;
         this.active = active;
     }
 }
@@ -21,9 +24,64 @@ function initSpreadsheet() {
         let spreadsheetRow = [];
 
         for (let j = 0; j < COLS; j++) {
-            spreadsheetRow.push(i + "-" + j);
+            let cellData = "";
+            let isHeader = false;
+            let disabled = false;
+
+            // 모든 첫 번째 컬럼에 숫자 넣기
+            if (j === 0) {
+                cellData = i;
+                isHeader = true;
+                disabled = true;
+            }
+
+            if (i === 0) {
+                cellData = alphabets[j - 1];
+                isHeader = true;
+                disabled = true;
+            }
+
+            // 첫 번째 row의 컬럼은 ""
+            if (!cellData) {
+                cellData = "";
+            }
+
+            const rowName = i;
+            const columnName = alphabets[j - 1];
+
+            const cell = new Cell(isHeader, disabled, cellData, i, j, false);
+            spreadsheetRow.push(cell);
         }
 
         spreadsheet.push(spreadsheetRow);
+    }
+    drawSheet();
+    console.log(spreadsheet);
+}
+
+function createCellEl(cell) {
+    const cellEl = document.createElement('input');
+    cellEl.className = 'cell';
+    cellEl.id = 'cell_' + cell.row + cell.column;
+    cellEl.value = cell.data;
+    cellEl.disabled = cell.disabled;
+
+    if (cell.isHeader) {
+        cellEl.classList.add("header");
+    }
+
+    return cellEl;
+}
+
+function drawSheet() {
+    for (let i = 0; i < spreadsheet.length; i++) {
+        const rowContainerEl = document.createElement("div");
+        rowContainerEl.className = "cell-row";
+
+        for (let j = 0; j < spreadsheet[i].length; j++) {
+            const cell = spreadsheet[i][j];
+            rowContainerEl.append(createCellEl(cell));
+        }
+        spreadSheetContainer.append(rowContainerEl);
     }
 }
